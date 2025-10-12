1b4a90a1799d46b1b63ba391465832064520ac01
minthantko <minthantko101@gmail.com>
2025-10-12 14:45:34 +0700

    initialize with dependencies

---
+++ src/main/java/se331/project/AntiFakeNewsPjBackendApplication.java
@@ -0,0 +1,13 @@
+package se331.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFakeNewsPjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiFakeNewsPjBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se331/project/AntiFakeNewsPjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiFakeNewsPjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

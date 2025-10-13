d00c9fd7f9ff2873491dea370ec3e64ba6639e28
Cinnamonroll-afk <monworkonly@gmail.com>
2025-10-13 10:52:49 +0700

    add_dto_Newservice

--- src/main/java/se331/project/service/NewsService.java
+++ src/main/java/se331/project/service/NewsService.java
@@ -3,13 +3,14 @@ package se331.project.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import se331.project.dto.NewsDto;
 import se331.project.entity.News;
 
 
 // this i will use to pull data and send to frontend
 public interface NewsService {
-    Page<News> getNews(String status, String search, Pageable pageable);
-    News getNewsById(Long id);
+    Page<NewsDto> getNews(String status, String search, Pageable pageable);
+    NewsDto getNewsById(Long id);
     News save(News news);
     void deleteById(Long id);
 }

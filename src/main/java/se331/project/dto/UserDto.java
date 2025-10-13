8d10640d2bbbd2a0c6861b97bb698a2184d8561d
Cinnamonroll-afk <monworkonly@gmail.com>
2025-10-13 10:19:06 +0700

    add_UserSimpleDto

---
+++ src/main/java/se331/project/dto/UserDto.java
@@ -0,0 +1,16 @@
+package se331.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    Long id;
+    String firstname;
+    String lastname;
+}
